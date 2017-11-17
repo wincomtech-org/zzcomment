8666ba0627c91b44ad4b53d1246abed91bf53231
11223ren <32731986+11223ren@users.noreply.github.com>
2017-11-17 16:17:54 +0800

    s
    s

--- alipay/config.php
+++ alipay/config.php
@@ -1,7 +1,7 @@
 <?php
 header("content-type:text/html;charset=utf-8");
 ini_set('date.timezone','Asia/Shanghai');
-$config=(require_once dirname(dirname(__FILE__)).'/data/Conf/db.php');
+$config=(require_once dirname(dirname(__FILE__)).'/data/conf/db.php');
 $alipay_config=$config['ALIPAY_CONFIG'];
 $mysqli=new mysqli($config['DB_HOST'], $config['DB_USER'], $config['DB_PWD'], $config['DB_NAME'],$config['DB_PORT']);
 $mysqli->set_charset('utf8');
