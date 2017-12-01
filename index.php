6b907681313bf668f98f6d0ee2abbfa0cdf1582e
11223ren <32731986+11223ren@users.noreply.github.com>
2017-12-01 14:54:45 +0800

    关闭调试
    关闭调试

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
  */
 if (ini_get('magic_quotes_gpc')) {
 	function stripslashesRecursive(array $array){
-		foreach ($array as $k => $v) {
+		 foreach ($array as $k => $v) {
 			if (is_string($v)){
 				$array[$k] = stripslashes($v);
 			} else if (is_array($v)){
 				$array[$k] = stripslashesRecursive($v);
 			}
-		}
+		} 
 		return $array;
 	}
+	
 	$_GET = stripslashesRecursive($_GET);
 	$_POST = stripslashesRecursive($_POST);
 }
