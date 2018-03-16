ab187f25c3229b6e2bb00bb5dc3b7b60b60b68a1
11223ren <1132974092@qq.com>
2018-03-16 11:46:12 +0800

    修改系统任务代码

--- time.php
+++ time.php
@@ -1,8 +1,9 @@
 <?php
+//系统任务，在linux中crontab设置每日0点1分启动
 date_default_timezone_set('PRC');
 header("content-type:text/html;charset=utf-8");
 set_time_limit(600);
-$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+// $url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 $log='1.log';
 error_log(date('Y-m-d H:i:s')."\r\n",'3',$log);
 
@@ -29,13 +30,14 @@ $sql="update cm_top_seller set status=3 where status=2 and end_time<{$time}";
 $db->query($sql);
 $row=$db->affected_rows;
 error_log(date('Y-m-d H:i:s')."检查店铺置顶过期，改变了".$row."行\r\n",'3',$log);
-
-$today=date('Y-m-d',$time);
-$sleep=strtotime($today)+3600*24+2-$time;
-error_log(date('Y-m-d H:i:s')."sleep时间".($sleep/3600)."小时\r\n",'3',$log);
-sleep($sleep);
-error_log(date('Y-m-d H:i:s')."\r\n",'3',$log);
-file_get_contents($url);
+$db->close(); 
+error_log(date('Y-m-d H:i:s')."系统任务执行完成\r\n",'3',$log);
+// $today=date('Y-m-d',$time);
+// $sleep=strtotime($today)+3600*24+2-$time;
+// error_log(date('Y-m-d H:i:s')."sleep时间".($sleep/3600)."小时\r\n",'3',$log);
+// sleep($sleep);
+// error_log(date('Y-m-d H:i:s')."\r\n",'3',$log);
+// file_get_contents($url);
 
 exit;
 ?>
